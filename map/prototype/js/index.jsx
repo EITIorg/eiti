991f40e8ee03dce0297633d6dcf585280776b2cb
Fernando Ferreyra <fferreyra@developmentgateway.org>
2016-02-17 22:43:57 -0500

    updated map prototype and related files

--- map/prototype/js/index.jsx
+++ map/prototype/js/index.jsx
@@ -26,24 +26,33 @@ window.mapWidget.createHomePage = function(options) {
 
 
 window.mapWidget.createMapPage = function(options) {
-
+  var formatnumber =function(number) {
+    if(isNaN(number)) return 0;
+    let n = 0;
+    let x = 3;
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+    return number.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
+};
   var showTooltip = function (e) {
       var layer = e.target;
       //debugger;
       var country = countryStatus.find(function(v){return v.ISO3===layer.feature.id});
       if(country === undefined || country.EITI_Status == 4) return;
-      var html = '<div>'+
-        '<table class="country_info">'+
-        '  <thead><tr style="background-color:#f4f4f4"><th colspan="3"><img src="img/flags/gif/'  + layer.feature.id.toLowerCase() + '.gif" style="margin:0px 10px 0px 5px"/>'  + layer.feature.properties.name + '</th></tr></thead>'+
-        '  <tbody>'+
-        '    <tr><td colspan="2">GDP: 32.69 Billion USD (2013)</td><td>Population: 67.51 million</td></tr>'+
-        '    <tr><td colspan="3">&nbsp;<strong>Country Commodity Total</strong><hr style="margin:2px;border-color:#77cde4"/></td></tr>'+
-        '    <tr><td><strong>123</strong><img src="img/oil1.gif" style="margin:0px 2px 0px 2px"/> Crude Oil</td><td><strong>123</strong><img src="img/oil2.gif" style="margin:0px 2px 0px 2px"/> Refined Oil</td><td><strong>123</strong><img src="img/oil3.gif" style="margin:0px 2px 0px 2px"/> Other</td></tr>'+
-        '    <tr><td><strong>45</strong><img src="img/min1.gif" style="margin:0px 2px 0px 2px"/> Mineral</td><td></td><td></td></tr>'+
-        '    <tr><td colspan="3"><hr style="margin:2px;border-color:#77cde4"/><img src="img/cp.gif" style="margin:0px 2px 0px 2px"/> <a href="countries.html"style="color:#0b82d6">Open Country Page </td></tr>'+
-        '  </tbody>'+
-        '</table>'+
+
+      var html = '<div>' +
+        '<table class="country_info">' + 
+        '  <thead><tr style="background-color:#f4f4f4"><th colspan="3"><img src="img/flags/gif/' + layer.feature.id.toLowerCase() + '.gif" style="margin:0px 10px 0px 5px"/>' + layer.feature.properties.name + '</th></tr></thead>' + 
+        '  <tbody>' + '    <tr><td colspan="2">GDP: ' + formatnumber(country.GDP) + ' </td><td>Population: ' + formatnumber(country.Pop) + '</td></tr>' + 
+        '    <tr><td colspan="3">&nbsp;<strong>Country Commodity Total</strong><hr style="margin:2px;border-color:#77cde4"/></td></tr>' + 
+        '    <tr><td><strong>' + formatnumber(country.oil) + '</strong><img src="images/icon-dump/eiti_popup_oilunrefined.svg" style="margin:0px 2px 0px 2px;width:18px;"/> Crude Oil</td>' + 
+        '        <td><strong>' + country.refined + '</strong><img src="images/icon-dump/eiti_popup_oilrefined.svg" style="margin:0px 2px 0px 2px;width:18px;"/> Refined Oil</td>' + 
+        '        <td><strong>' + country.other + '</strong><img src="images/icon-dump/eiti_popup_other.svg" style="margin:0px 2px 0px 2px;width:18px"/> Other</td></tr>' + 
+        '    <tr><td><strong>' + country.mineral + '</strong><img src="images/icon-dump/eiti_popup_mineral.svg" style="margin:0px 2px 0px 2px;width:18px"/> Mineral</td>' + 
+        '        <td></td><td></td></tr>' + '    <tr><td colspan="3"><hr style="margin:2px;border-color:#77cde4"/><img src="images/icon-dump/eiti_popup_opencountry.svg" style="margin:0px 2px 0px 2px;width:18px"/> <a href="countries.html' + country.ISO3 + '" style="color:#0b82d6">Open Country Page </td></tr>' + 
+        '  </tbody>' + 
+        '</table>' + 
         '</div>';
+
       var popup = L.popup({autoPan:true, closeButton:false, maxWidth:400})
         .setLatLng(e.latlng)
         .setContent(html)//'<strong>' + layer.feature.properties.name + '</strong>');
