fdfeb89df6f4540907d08501a07a2117a8405420
Schneider Werner-Walter <wschneider@developmentgateway.org>
2016-03-04 16:11:15 +0200

    EITI-284: Update sime-interactive map style.

--- public_html/sites/all/modules/custom/eiti_ctools_extra/plugins/content_types/semi-interactive-map/semi-interactive-map.init.js
+++ public_html/sites/all/modules/custom/eiti_ctools_extra/plugins/content_types/semi-interactive-map/semi-interactive-map.init.js
@@ -8,13 +8,29 @@
         return;
       }
 
+      var map_container = $('#' + settings.semiInteractiveMap.container, context);
+      if (!map_container.length) {
+        return;
+      }
+
+      var default_zoom = 2;
+
+      // Set the default zoom based on the map size.
+      var map_container_width = map_container.width();
+      if (map_container_width <= 480) {
+        default_zoom = 0;
+      }
+      else if (map_container_width > 480 && map_container_width <= 800) {
+        default_zoom = 1;
+      }
+
       mapWidget.createHomePage({
         name: Drupal.t('Homepage Map'),
         container: settings.semiInteractiveMap.container,
         position: settings.semiInteractiveMap.position,
-        zoom: 2,
+        zoom: default_zoom,
         maxZoom: 5,
-        minZoom: 2
+        minZoom: 0
       });
     }
   };
