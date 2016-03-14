18cca771b20259b640053361c3b11bcd0bf41bea
Schneider Werner-Walter <wschneider@developmentgateway.org>
2016-03-14 17:32:07 +0200

    EITI-294: Updated the indicators

--- public_html/sites/default/update_scripts/temporary/20160311.294.01.php
+++ public_html/sites/default/update_scripts/temporary/20160311.294.01.php
@@ -8,7 +8,7 @@ $indicators_to_text = array(
 
 $query = db_update('eiti_indicator');
 $query->fields(array(
-  'type' => 'boolean',
+  'type' => 'text',
 ));
 $query->condition('name', $indicators_to_text, 'IN');
 $query->execute();
