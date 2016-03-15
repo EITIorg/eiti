3a94a5825875731ed3de6628c1ce0975f1ef0074
Schneider Werner-Walter <wschneider@developmentgateway.org>
2016-03-15 14:34:35 +0200

    EITI-139: Add "Related Country" field facet on the search page.

--- public_html/sites/all/themes/eiti/templates/views-view--search--page.tpl.php
+++ public_html/sites/all/themes/eiti/templates/views-view--search--page.tpl.php
@@ -39,52 +39,54 @@
     </div>
   <?php endif; ?>
 
-  <?php if ($exposed): ?>
-    <div class="view-filters">
-      <?php print $exposed; ?>
-    </div>
-  <?php endif; ?>
-
   <?php if ($attachment_before): ?>
-    <div class="attachment attachment-before">
+    <div class="attachment attachment-before attachment-column-wrapper">
       <?php print $attachment_before; ?>
     </div>
   <?php endif; ?>
 
-  <?php if ($rows): ?>
-    <div class="view-content">
-      <?php print $rows; ?>
-    </div>
-  <?php elseif ($empty): ?>
-    <div class="view-empty">
-      <?php print $empty; ?>
-    </div>
-  <?php endif; ?>
+  <div class="main-column-wrapper">
+    <?php if ($exposed): ?>
+      <div class="view-filters">
+        <?php print $exposed; ?>
+      </div>
+    <?php endif; ?>
 
-  <?php if ($pager): ?>
-    <?php print $pager; ?>
-  <?php endif; ?>
+    <?php if ($rows): ?>
+      <div class="view-content">
+        <?php print $rows; ?>
+      </div>
+    <?php elseif ($empty): ?>
+      <div class="view-empty">
+        <?php print $empty; ?>
+      </div>
+    <?php endif; ?>
 
-  <?php if ($attachment_after): ?>
-    <div class="attachment attachment-after">
-      <?php print $attachment_after; ?>
-    </div>
-  <?php endif; ?>
+    <?php if ($pager): ?>
+      <?php print $pager; ?>
+    <?php endif; ?>
 
-  <?php if ($more): ?>
-    <?php print $more; ?>
-  <?php endif; ?>
+    <?php if ($attachment_after): ?>
+      <div class="attachment attachment-after">
+        <?php print $attachment_after; ?>
+      </div>
+    <?php endif; ?>
 
-  <?php if ($footer): ?>
-    <div class="view-footer">
-      <?php print $footer; ?>
-    </div>
-  <?php endif; ?>
+    <?php if ($more): ?>
+      <?php print $more; ?>
+    <?php endif; ?>
 
-  <?php if ($feed_icon): ?>
-    <div class="feed-icon">
-      <?php print $feed_icon; ?>
-    </div>
-  <?php endif; ?>
+    <?php if ($footer): ?>
+      <div class="view-footer">
+        <?php print $footer; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($feed_icon): ?>
+      <div class="feed-icon">
+        <?php print $feed_icon; ?>
+      </div>
+    <?php endif; ?>
+  </div>
 
 </div><?php /* class view */ ?>
