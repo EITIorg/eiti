26c3cb66bdcb00ce7bd17f2dd496f25c6f62adc4
Schneider Werner-Walter <wschneider@developmentgateway.org>
2016-03-30 17:51:37 +0300

    EITI-94: Replace admin pages with views that allows site administrators to search for content in listings.

---
+++ public_html/sites/default/update_scripts/temporary/20160325.94.03.php
@@ -0,0 +1,10 @@
+<?php
+
+// Provide a list of modules to be installed.
+$modules = array(
+  'admin_views',
+
+  'views_bulk_operations',
+    'actions_permissions',
+);
+_us_module__install($modules);
