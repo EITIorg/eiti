f564f632602bc561398d575d847eda8b5f245a9c
Schneider Werner-Walter <wschneider@developmentgateway.org>
2016-03-30 18:38:42 +0300

    EITI-94: Prepare update scripts for release.

--- public_html/sites/default/update_scripts/temporary/20160325.94.02.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Provide a list of modules to be installed.
-$modules = array(
-  'shortcut',
-);
-_us_module__install($modules);
--- public_html/sites/default/update_scripts/temporary/20160328.333.01.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// Use adminimal as the administration theme.
-variable_set('admin_theme', 'adminimal');
