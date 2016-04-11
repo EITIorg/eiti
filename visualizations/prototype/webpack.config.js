536b8b5d56d32c6393a27de906191a6a032f1f5a
Schneider Werner-Walter <wschneider@developmentgateway.org>
2016-04-11 12:08:18 +0300

    EITI-423: Update chartWidgets webpack.config.js whitespace.

--- visualizations/prototype/webpack.config.js
+++ visualizations/prototype/webpack.config.js
@@ -2,33 +2,33 @@ var webpack = require("webpack");
 var	path = require("path");
 
 module.exports = {
-	entry: {
-		"chartWidgets": path.resolve(__dirname, "js/index.jsx"),
-		"chartWidgets.min": path.resolve(__dirname, "js/index.jsx")
-	},
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: "[name].js"
-	},
-	plugins: [
-    	new webpack.HotModuleReplacementPlugin(),
-    	new webpack.NoErrorsPlugin(),
-    	new webpack.optimize.UglifyJsPlugin({
-	      include: /\.min\.js$/,
-	      minimize: true
-	    })
-    ],
-	module: {
-		loaders: [{
-			test: /\.(js|jsx|es6)$/,
-			loaders: ['babel-loader']
-		}, {
-			test: /\.jpe?g$|\.gif$|\.eot$|\.png$|\.svg$|\.woff$|\.woff2$|\.ttf$|\.wav$|\.mp3$/,
-			loader: "file"
-		}
-		]
-	},
-	resolve: {
-		extensions: ["", ".webpack.js", ".web.js", ".js", ".jsx",".es6"]
-	}
+  entry: {
+    "chartWidgets": path.resolve(__dirname, "js/index.jsx"),
+    "chartWidgets.min": path.resolve(__dirname, "js/index.jsx")
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "[name].js"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.(js|jsx|es6)$/,
+      loaders: ['babel-loader']
+    }, {
+      test: /\.jpe?g$|\.gif$|\.eot$|\.png$|\.svg$|\.woff$|\.woff2$|\.ttf$|\.wav$|\.mp3$/,
+      loader: "file"
+    }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".js", ".jsx",".es6"]
+  }
 };
