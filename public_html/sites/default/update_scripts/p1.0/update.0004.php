911703c0d5414cefd74347bb8f5a0c6f10ad7d65
Schneider Werner-Walter <wschneider@developmentgateway.org>
2016-04-21 10:34:27 +0300

    EITI-407: Update styling for Article entities.

--- public_html/sites/default/update_scripts/p1.0/update.0004.php
+++ public_html/sites/default/update_scripts/p1.0/update.0004.php
@@ -18,3 +18,11 @@ variable_set('date_first_day', 1);
 variable_set('date_format_long', 'l, F j, Y - H:i');
 variable_set('date_format_medium', 'D, Y-m-d H:i');
 variable_set('date_format_short', 'm/d/Y - H:i');
+
+// Add a custom date format "Feb 28, 2012".
+$date_format = 'M j, Y';
+_us_system__add_date_format($date_format);
+
+$machine_name = 'shortest';
+$title = 'Shortest';
+_us_system__add_date_format_type($machine_name, $title, $date_format);
