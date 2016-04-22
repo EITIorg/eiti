95f2a5bb97e2209dc38e490f6591f10eae6f562b
Schneider Werner-Walter <wschneider@developmentgateway.org>
2016-04-22 14:12:40 +0300

    EITI-441: Enable per entity content access for Page node bundle.

--- public_html/sites/default/update_scripts/temporary/20160413.441.01.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Provide a list of CORE modules to be disabled and uninstalled.
-$modules = array(
-  'content_access',
-);
-_us_module__uninstall($modules);
---
+++ public_html/sites/default/update_scripts/temporary/20160413.441.02.php
@@ -0,0 +1,7 @@
+<?php
+
+// Provide a list of modules to be enabled and installed.
+$modules = array(
+  'content_access',
+);
+_us_module__install($modules);
