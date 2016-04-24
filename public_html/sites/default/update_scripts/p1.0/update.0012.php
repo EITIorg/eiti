10853a010c5daa05921d9ce296ffae489b534688
Schneider Werner-Walter <wschneider@developmentgateway.org>
2016-04-24 14:58:59 +0300

    EITI-416: Fix blank taxonomy name field (title.module).

--- public_html/sites/default/update_scripts/p1.0/update.0012.php
+++ public_html/sites/default/update_scripts/p1.0/update.0012.php
@@ -1,30 +1,19 @@
 <?php
 
 // Let's create default taxonomy terms for "Country Status".
-$default_terms_en = array(
+
+// Prepare a list of ordered taxonomy terms.
+$types = array(
   'Compliant',
   'Candidate',
   'Suspended',
   'Other',
 );
-
-$vocabulary_name = 'country_status';
-$vocabulary = taxonomy_vocabulary_machine_name_load($vocabulary_name);
-$language = 'en';
-
-if (!empty($vocabulary)) {
-  foreach ($default_terms_en as $weight => $term_name) {
-    $existing = taxonomy_get_term_by_name($term_name, $vocabulary_name);
-    if (!empty($existing)) {
-      continue;
-    }
-
-    $term = new stdClass();
-    $term->name = $term_name;
-    $term->vid = $vocabulary->vid;
-    $term->weight = $weight;
-    $term->language = $language;
-
-    taxonomy_term_save($term);
-  }
+foreach ($types as $weight => $name) {
+  $term = new stdClass();
+  $term->name = $name;
+  $term->weight = $weight;
+  $term->language = 'en';
+  $term->vocabulary_machine_name = 'country_status';
+  _us_taxonomy__term_save($term);
 }
