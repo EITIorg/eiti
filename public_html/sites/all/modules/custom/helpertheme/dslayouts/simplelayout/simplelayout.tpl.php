4f20b88ed5f83b7f31bca234551219acb6a77dc3
Schneider Werner-Walter <wschneider@developmentgateway.org>
2016-05-05 02:59:55 +0300

    EITI-472: Fix markup for widgets with background image.

--- public_html/sites/all/modules/custom/helpertheme/dslayouts/simplelayout/simplelayout.tpl.php
+++ public_html/sites/all/modules/custom/helpertheme/dslayouts/simplelayout/simplelayout.tpl.php
@@ -13,10 +13,10 @@
 
   <<?php print $main_wrapper; if (isset($main_attributes)) { print drupal_attributes($main_attributes);} ?> class="group-main <?php print $main_classes; ?>">
     <?php print $main; ?>
-  </<?php print $main_wrapper ?>>
 
-  <?php if (!empty($drupal_render_children)): ?>
-    <?php print $drupal_render_children ?>
-  <?php endif; ?>
+    <?php if (!empty($drupal_render_children)): ?>
+      <?php print $drupal_render_children ?>
+    <?php endif; ?>
+  </<?php print $main_wrapper ?>>
 
 </<?php print $layout_wrapper ?>>
