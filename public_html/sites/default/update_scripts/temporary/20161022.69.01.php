c9e0b1caeb7d225fb9492394c06368c0a3bdbfc3
Sergiu Nagailic | Nikro <nikro.md@gmail.com>
2016-10-22 01:27:24 -0500

    EITIMS-69: Updated the temporary update script.

--- public_html/sites/default/update_scripts/temporary/20161022.69.01.php
+++ public_html/sites/default/update_scripts/temporary/20161022.69.01.php
@@ -1,5 +1,11 @@
 <?php
 
+// Provide a list of modules to be installed.
+$modules = array(
+  'eitientity_score_data',
+);
+_us_module__install($modules);
+
 // Prepare a list of features to be installed.
 $feature_names = array(
   'eitiet_score_data',
