ad9045b007575825d10d4dfef3c21e570ae45134
Fernando Ferreyra <fferreyra@developmentgateway.org>
2016-10-24 17:17:41 -0400

    EITIMS-71 initial draft version

--- visualizations/chartWidgets/source/scripts/index.jsx
+++ visualizations/chartWidgets/source/scripts/index.jsx
@@ -10,6 +10,7 @@ let StackedBarExport = require('./chartWidgets/StackedBarExport');
 let TreeMap = require('./chartWidgets/TreeMap');
 let BubbleChart = require('./chartWidgets/BubbleChart');
 let Sankey = require('./chartWidgets/Sankey');
+let Scorecard = require('./chartWidgets/Scorecard');
 
 import { render } from 'react-dom';
 
@@ -21,142 +22,169 @@ window.chartWidget.create = function(options, data) {
 	else { //assumption: data is tabular JSON data
 		chartData = data;
 	}
-	if(options.type == "Bar") {
-		render((
-			<div className={options.className}>
-				<h3>{options.name}</h3>
-				<BarChart
-					width = {options.width}
-					height = {options.height}
-					margin = {options.margin}
-					xlabel = {options.xlabel}
-	            	ylabel = {options.ylabel}
-	            	dataURL = {dataURL}
-	            	chartData = {chartData}
-	            	processor = {options.processor}
-	           	/>
-	        </div>
-		), document.getElementById(options.container))
-	} else if(options.type == "Pie") {
-		render((
-			<div className={options.className}>
-				<h3>{options.name}</h3>
-				<PieChart
-					width = {options.width}
-					height = {options.height}
-					margin = {options.margin}
-	            	dataURL = {dataURL}
-	            	chartData = {chartData}
-	            	processor = {options.processor}
-	           	/>
-	        </div>
-		), document.getElementById(options.container))
-	} else if(options.type == "GroupedBar") {
-		render((
-			<div className={options.className}>
-				<GroupedBar
-					chartTitle = {options.name}
-					width = {options.width}
-					height = {options.height}
-					margin = {options.margin}
-					xlabel = {options.xlabel}
-	            	ylabel = {options.ylabel}
-	            	dataURL = {dataURL}
-	            	chartData = {chartData}
-	            	legend = {true}
-	            	legendClass = {options.legendClass}
-	            	processor = {options.processor}
-	           	/>
-	           	<h4 className={'chartDescription'}>{options.description}</h4>
-	        </div>
-		), document.getElementById(options.container))
-	} else if(options.type == "GroupedBarExport") {
-		render((
-			<div className={options.className}>
-				<GroupedBarExport
-					xlabel = {options.xlabel}
-	            	dataURL = {dataURL}
-	            	chartData = {chartData}
-	            	processor = {options.processor}
-	           	/>
-	        </div>
-		), document.getElementById(options.container))
-	} else if(options.type == "StackedBar") {
-		render((
-			<div className={options.className}>
-				<StackedBar
-					chartTitle = {options.name}
-					width = {options.width}
-					height = {options.height}
-					margin = {options.margin}
-					xlabel = {options.xlabel}
-	            	ylabel = {options.ylabel}
-	            	dataURL = {dataURL}
-	            	chartData = {chartData}
-	            	legend = {true}
-	            	legendClass = {options.legendClass}
-	            	processor = {options.processor}
-	           	/>
-	           	<h4 className={'chartDescription'}>{options.description}</h4>
-	        </div>
-		), document.getElementById(options.container))
-	} else if(options.type == "StackedBarExport") {
-		render((
-			<div className={options.className}>
-				<StackedBarExport
-					xlabel = {options.xlabel}
-	            	dataURL = {dataURL}
-	            	chartData = {chartData}
-	            	processor = {options.processor}
-	           	/>
-	        </div>
-		), document.getElementById(options.container))
-	} else if(options.type == "TreeMap") {
-		render((
-			<div className={options.className}>
-				<TreeMap
-					chartTitle = {options.name}
-					width = {options.width}
-					height = {options.height}
-					margin = {options.margin}
-	            	dataURL = {dataURL}
-	            	chartData = {chartData}
-	            	processor = {options.processor}
-				/>
-				<h4 className={'chartDescription'}>{options.description}</h4>
-			</div>
-		), document.getElementById(options.container))
-	} else if(options.type == "BubbleChart") {
-		render((
-			<div className={options.className}>
-				<BubbleChart
-					chartTitle = {options.name}
-					width = {options.width}
-					height = {options.height}
-					diameter = {options.diameter}
-					margin = {options.margin}
-	            	dataURL = {dataURL}
-	            	chartData = {chartData}
-	            	processor = {options.processor}
-				/>
-				<h4 className={'chartDescription'}>{options.description}</h4>
-			</div>
-		), document.getElementById(options.container))
-	} else if(options.type == "Sankey") {
-		render((
-			<div className={options.className}>
-				<Sankey
-					chartTitle = {options.name}
-					width = {options.width}
-					height = {options.height}
-					margin = {options.margin}
-	            	dataURL = {dataURL}
-	            	chartData = {chartData}
-	            	processor = {options.processor}
-				/>
-				<h4 className={'chartDescription'}>{options.description}</h4>
-			</div>
-		), document.getElementById(options.container))
+
+	switch(options.type) {
+		case "Bar": 
+			render((
+				<div className={options.className}>
+					<h3>{options.name}</h3>
+					<BarChart
+						width = {options.width}
+						height = {options.height}
+						margin = {options.margin}
+						xlabel = {options.xlabel}
+		            	ylabel = {options.ylabel}
+		            	dataURL = {dataURL}
+		            	chartData = {chartData}
+		            	processor = {options.processor}
+		           	/>
+		        </div>
+			), document.getElementById(options.container))
+		break;
+		case "Pie": 
+			render((
+				<div className={options.className}>
+					<h3>{options.name}</h3>
+					<PieChart
+						width = {options.width}
+						height = {options.height}
+						margin = {options.margin}
+		            	dataURL = {dataURL}
+		            	chartData = {chartData}
+		            	processor = {options.processor}
+		           	/>
+		        </div>
+			), document.getElementById(options.container))
+		break;
+		case "GroupedBar": 
+			render((
+				<div className={options.className}>
+					<GroupedBar
+						chartTitle = {options.name}
+						width = {options.width}
+						height = {options.height}
+						margin = {options.margin}
+						xlabel = {options.xlabel}
+		            	ylabel = {options.ylabel}
+		            	dataURL = {dataURL}
+		            	chartData = {chartData}
+		            	legend = {true}
+		            	legendClass = {options.legendClass}
+		            	processor = {options.processor}
+		           	/>
+		           	<h4 className={'chartDescription'}>{options.description}</h4>
+		        </div>
+			), document.getElementById(options.container))
+		break;
+		case "GroupedBarExport": 
+			render((
+				<div className={options.className}>
+					<GroupedBarExport
+						xlabel = {options.xlabel}
+		            	dataURL = {dataURL}
+		            	chartData = {chartData}
+		            	processor = {options.processor}
+		           	/>
+		        </div>
+			), document.getElementById(options.container))
+		break;
+		case "StackedBar": 
+			render((
+				<div className={options.className}>
+					<StackedBar
+						chartTitle = {options.name}
+						width = {options.width}
+						height = {options.height}
+						margin = {options.margin}
+						xlabel = {options.xlabel}
+		            	ylabel = {options.ylabel}
+		            	dataURL = {dataURL}
+		            	chartData = {chartData}
+		            	legend = {true}
+		            	legendClass = {options.legendClass}
+		            	processor = {options.processor}
+		           	/>
+		           	<h4 className={'chartDescription'}>{options.description}</h4>
+		        </div>
+			), document.getElementById(options.container))
+		break;
+		case "StackedBarExport": 
+			render((
+				<div className={options.className}>
+					<StackedBarExport
+						xlabel = {options.xlabel}
+		            	dataURL = {dataURL}
+		            	chartData = {chartData}
+		            	processor = {options.processor}
+		           	/>
+		        </div>
+			), document.getElementById(options.container))
+		break;
+		case "TreeMap": 
+			render((
+				<div className={options.className}>
+					<TreeMap
+						chartTitle = {options.name}
+						width = {options.width}
+						height = {options.height}
+						margin = {options.margin}
+		            	dataURL = {dataURL}
+		            	chartData = {chartData}
+		            	processor = {options.processor}
+					/>
+					<h4 className={'chartDescription'}>{options.description}</h4>
+				</div>
+			), document.getElementById(options.container))
+		break;
+		case "BubbleChart": 
+			render((
+				<div className={options.className}>
+					<BubbleChart
+						chartTitle = {options.name}
+						width = {options.width}
+						height = {options.height}
+						diameter = {options.diameter}
+						margin = {options.margin}
+		            	dataURL = {dataURL}
+		            	chartData = {chartData}
+		            	processor = {options.processor}
+					/>
+					<h4 className={'chartDescription'}>{options.description}</h4>
+				</div>
+			), document.getElementById(options.container))
+		break;
+		case "Sankey": 
+			render((
+				<div className={options.className}>
+					<Sankey
+						chartTitle = {options.name}
+						width = {options.width}
+						height = {options.height}
+						margin = {options.margin}
+		            	dataURL = {dataURL}
+		            	chartData = {chartData}
+		            	processor = {options.processor}
+					/>
+					<h4 className={'chartDescription'}>{options.description}</h4>
+				</div>
+			), document.getElementById(options.container))
+		break;
+		case "Scorecard": 
+			render((
+				<div className={options.className}>
+					<Scorecard
+						chartTitle = {options.name}
+						width = {options.width}
+						height = {options.height}
+						margin = {options.margin}
+		            	dataURL = {dataURL}
+		            	chartData = {chartData}
+		            	processor = {options.processor}
+					/>
+					<h4 className={'chartDescription'}>{options.description}</h4>
+				</div>
+			), document.getElementById(options.container))
+		break;
 	}
 }
 
