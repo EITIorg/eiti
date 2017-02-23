1fc3543bf1123ad07301d157814565abbc50e51f
Sergiu Nagailic | Nikro <nikro.md@gmail.com>
2017-02-23 17:44:51 +0200

    EITIMS-98: Split the update script.

--- public_html/sites/default/update_scripts/p1.0/update.0028.php
+++ public_html/sites/default/update_scripts/p1.0/update.0028.php
@@ -15,9 +15,3 @@ $modules = array(
     'xmlsitemap_engines',
 );
 _us_module__install($modules);
-
-// Prepare a list of features to be installed.
-$feature_names = array(
-  'eitict_progress_report',
-);
-_us_features__install($feature_names);
