14b0ec15c9cc9fd38bc12f206cba4951116405bc
Fernando Ferreyra <fferreyra@developmentgateway.org>
2017-06-13 14:24:11 -0400

    EITIMS-68 updated colors and legend

--- visualizations/mapWidgets/source/scripts/data/status.js
+++ visualizations/mapWidgets/source/scripts/data/status.js
@@ -1,39 +1,38 @@
 export var status = [
   {
     "id": 5329,
-    "color": "#6CBCE3",
+    "color": "",
     "title": "Yet to be assessed against the 2016 Standard<br/>",
     "subtitle": ""
   },
   {
     "id": 75,
-    "color": "#2D8B2A",
+    "color": "",
     "title": "Satisfactory progress",
     "subtitle": ""
   },
   {
     "id": 74,
-    "color": "#84AD42",
+    "color": "",
     "title": "Meaningful progress",
     "subtitle": ""
   },
   {
     "id": 5327,
-    "color": "#FAC433",
+    "color": "",
     "title": "Inadequate progress / suspended<br/>",
     "subtitle": ""
   },
   {
     "id": 8212,
-    "color": "#FAC433",
+    "color": "",
     "title": "Suspended due to political instability",
     "subtitle": ""
   },
   {
     "id": 8213,
-    "color": "#FAC433",
+    "color": "",
     "title": "Suspended for missing deadline",
     "subtitle": ""
   }
 ];
-
