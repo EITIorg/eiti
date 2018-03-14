03be96c1e0f1816105331345fa5f88d4525097b3
Madis Kure <kmadis@gmail.com>
2018-03-14 14:09:27 +0200

    EITIMS-41 Composer, mpdf and eiti_pdf module with initial (unfinished) functionality

---
+++ public_html/sites/all/modules/custom/eiti_pdf/templates/eiti-pdf-scorecard.tpl.php
@@ -0,0 +1,23 @@
+<table class="country_scorecard">
+  <thead>
+    <tr>
+      <th colspan="2"><?php print t('EITI Requirements'); ?></th>
+      <th colspan="5"><?php print t('Level of Progress'); ?></th>
+      <?php if ($hasProgress): ?>
+        <th colspan="2"><?php print t('Direction <br>of Progress'); ?></th>
+      <?php endif; ?>
+    </tr>
+    <tr>
+      <th><?php print t('Categories'); ?></th>
+      <th><?php print t('Requirements'); ?></th>
+      <th class="scores"><?php print t('<div><span>No Progress</span></div>'); ?></th>
+      <th class="scores"><?php print t('<div><span>Inadequate</span></div>'); ?></th>
+      <th class="scores"><?php print t('<div><span>Meaningful</span></div>'); ?></th>
+      <th class="scores"><?php print t('<div><span>Satisfactory</span></div>'); ?></th>
+      <th class="scores"><?php print t('<div><span>Beyond</span></div>'); ?></th>
+    </tr>
+  </thead>
+  <tbody>
+
+  </tbody>
+</table>
