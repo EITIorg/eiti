cbfd8c13e38ec169e7afd4f427de1816c35aecb0
Madis Kure <kmadis@gmail.com>
2018-03-22 13:14:45 +0200

    EITIMS-41 Scorecard PDF html, css

--- public_html/sites/all/modules/custom/eiti_pdf/templates/eiti-pdf-scorecard.tpl.php
+++ public_html/sites/all/modules/custom/eiti_pdf/templates/eiti-pdf-scorecard.tpl.php
@@ -1,6 +1,6 @@
 <table class="country_scorecard">
   <thead>
-    <tr>
+    <tr class="first">
       <th colspan="2"><?php print t('EITI Requirements'); ?></th>
       <th colspan="5"><?php print t('Level of Progress'); ?></th>
       <?php if ($hasProgress): ?>
@@ -19,42 +19,32 @@
   </thead>
   <tbody>
   <?php foreach ($categories as $c_tid => $category): ?>
-    <?php $new_category = TRUE; ?>
-    <?php if (isset($requirements[$c_tid])): ?>
-      <?php foreach ($requirements[$c_tid] as $requirement): ?>
-      <tr>
-        <?php if ($new_category): ?>
-          <td class="requirement" style="border-bottom: 1px solid black;" rowspan="<?php print $category['count']; ?>">
-            <?php print $category['name']; ?>
-          </td>
-          <?php $new_category = FALSE; ?>
-        <?php endif; ?>
-        <td><?php print $requirement['name']; ?></td>
-        <?php if (!$requirement['is_applicable']): ?>
-          <td class="not_applicable" colspan="5">&nbsp;</td>
-        <?php elseif ($requirement['is_required']): ?>
-          <td class="only_encouraged" colspan="5">&nbsp;</td>
-        <?php else: ?>
-          <td<?php if ($requirement['value'] == 0): ?> class="no_progress"<?php endif; ?>>&nbsp;</td>
-          <td<?php if ($requirement['value'] == 1): ?> class="inadequate"<?php endif; ?>>&nbsp;</td>
-          <td<?php if ($requirement['value'] == 2): ?> class="meaningful"<?php endif; ?>>&nbsp;</td>
-          <td<?php if ($requirement['value'] == 3): ?> class="satisfactory"<?php endif; ?>>&nbsp;</td>
-          <td<?php if ($requirement['value'] == 4): ?> class="beyond"<?php endif; ?>>&nbsp;</td>
-        <?php endif; ?>
-        <?php if ($hasProgress): ?>
-          <?php if ($requirement['progress_value'] == 0): ?>
-            <td><div style="color: #676767; text-align: center; font-weight: bold;">&equals;</div></td>
-          <?php elseif ($requirement['progress_value'] == 1): ?>
-            <td><div style="color: #84AD42; text-align: center; font-weight: bold;">&rarr;</div></td>
-          <?php elseif ($requirement['progress_value'] == 2): ?>
-            <td><div style="color: red; text-align: center; font-weight: bold;">&larr;</div></td>
-          <?php else: ?>
-            <td><div style="color: #676767; text-align: center; font-weight: bold;">&nbsp;</div></td>
-          <?php endif; ?>
-        <?php endif; ?>
-      </tr>
-      <?php endforeach; ?>
-    <?php endif; ?>
+    <?php foreach ($requirements[$c_tid] as $requirement): ?>
+    <tr>
+      <?php if ($requirement['add_category_td']): ?>
+        <td class="requirement last" rowspan="<?php print $category['count']; ?>">
+          <?php print $category['name']; ?>
+        </td>
+      <?php endif; ?>
+      <td class="<?php print $requirement['td_classes']; ?>"><?php if ($requirement['print_name']): ?><?php print $requirement['name']; ?><?php endif; ?></td>
+      <?php if (!$requirement['is_applicable']): ?>
+        <td class="not_applicable <?php print $requirement['td_classes']; ?>" colspan="5"></td>
+      <?php elseif ($requirement['is_required']): ?>
+        <td class="only_encouraged <?php print $requirement['td_classes']; ?>" colspan="5"></td>
+      <?php else: ?>
+        <td class="<?php if ($requirement['value'] == 0): ?>no_progress<?php endif; ?> <?php print $requirement['td_classes']; ?>"></td>
+        <td class="<?php if ($requirement['value'] == 1): ?>inadequate<?php endif; ?> <?php print $requirement['td_classes']; ?>"></td>
+        <td class="<?php if ($requirement['value'] == 2): ?>meaningful<?php endif; ?> <?php print $requirement['td_classes']; ?>"></td>
+        <td class="<?php if ($requirement['value'] == 3): ?>satisfactory<?php endif; ?> <?php print $requirement['td_classes']; ?>"></td>
+        <td class="<?php if ($requirement['value'] == 4): ?>beyond<?php endif; ?> <?php print $requirement['td_classes']; ?>"></td>
+      <?php endif; ?>
+      <?php if ($hasProgress): ?>
+        <td class="<?php print $requirement['td_classes']; ?>">
+          <div class="<?php print $requirement['progress_classes']; ?>"><?php print $requirement['progress_symbol']; ?></div>
+        </td>
+      <?php endif; ?>
+    </tr>
+    <?php endforeach; ?>
   <?php endforeach; ?>
   </tbody>
 </table>
