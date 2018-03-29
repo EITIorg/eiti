cc2b0c459d7a3e343213e4b4c3f524a1df364ca6
Madis Kure <kmadis@gmail.com>
2018-03-29 17:10:56 +0300

    EITIMS-41 Stage 2: scorecard PDF title page (unfinished), filename

--- public_html/sites/all/modules/custom/eiti_pdf/templates/eiti-pdf-title-page.tpl.php
+++ public_html/sites/all/modules/custom/eiti_pdf/templates/eiti-pdf-title-page.tpl.php
@@ -1,9 +1,13 @@
 <div class="pdf-title-page">
-  <div class="logo">Logo</div>
-  <div class="line">
-    <span class="board"><?php print t('EITI Board'); ?></span>
-    <span class="date"><?php print $date; ?></span>
-  </div>
+  <div class="logo"><img src="<?php print $logo; ?>" alt="EITI" height="55" /></div>
+  <table class="line">
+    <tbody>
+      <tr>
+        <td class="board"><?php print t('EITI Board'); ?></td>
+        <td class="date"><?php print $date; ?></td>
+      </tr>
+    </tbody>
+  </table>
   <div class="title"><?php print $title; ?></div>
   <div class="reference"><?php print t('Decision reference:'); ?> <?php print $decision_reference; ?></div>
 </div>
