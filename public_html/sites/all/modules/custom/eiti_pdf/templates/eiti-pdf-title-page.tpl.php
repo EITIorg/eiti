3696066ae98d38a4bf7696b0ab18aec6ac09631f
Madis Kure <kmadis@gmail.com>
2018-03-29 15:16:13 +0300

    EITIMS-41 Stage 2: scorecard PDF title page (unfinished), text field format restrictions

---
+++ public_html/sites/all/modules/custom/eiti_pdf/templates/eiti-pdf-title-page.tpl.php
@@ -0,0 +1,9 @@
+<div class="pdf-title-page">
+  <div class="logo">Logo</div>
+  <div class="line">
+    <span class="board"><?php print t('EITI Board'); ?></span>
+    <span class="date"><?php print $date; ?></span>
+  </div>
+  <div class="title"><?php print $title; ?></div>
+  <div class="reference"><?php print t('Decision reference:'); ?> <?php print $decision_reference; ?></div>
+</div>
