2581f4cd51ff4d0391156306b99043ec8189e229
Madis Kure <kmadis@gmail.com>
2018-06-12 15:20:04 +0300

    EITIMS-8 Organisation, summary data (API) updates

--- public_html/sites/all/modules/custom/eiti_api/plugins/restful/organisation/1.0/EITIApiOrganisation.class.php
+++ public_html/sites/all/modules/custom/eiti_api/plugins/restful/organisation/1.0/EITIApiOrganisation.class.php
@@ -15,13 +15,69 @@ class EITIApiOrganisation extends RestfulEntityBase {
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
-    // Let's expose some data, as few as needed.
+    // Expose data.
     $public_fields['type'] = array(
       'property' => 'type',
     );
     $public_fields['country'] = array(
       'property' => 'country_id',
     );
+    $public_fields['summary_data'] = array(
+      'property' => 'summary_data_id',
+    );
+    $public_fields['identification'] = array(
+      'property' => 'identification_number',
+    );
+    $public_fields['sector'] = array(
+      'callback' => array($this, 'getSector'),
+    );
+    $public_fields['commodities'] = array(
+      'callback' => array($this, 'getCommodities'),
+    );
     return $public_fields;
+  }
+
+  /**
+   * Gets the sector related to this organisation.
+   *
+   * @param $emw
+   *   EntityMetadataWrapper object (Organisation).
+   *
+   * @return mixed
+   *   Either NULL or the sector string.
+   */
+  function getSector($emw) {
+    // Can't use property directly since government agencies do not have this field.
+    if (isset($emw->field_o_sector)) {
+      $sector = $emw->field_o_sector->value();
+      if ($sector) {
+        return $sector->name;
+      }
+    }
+    return NULL;
+  }
+
+  /**
+   * Gets the commodities related to this organisation.
+   *
+   * @param $emw
+   *   EntityMetadataWrapper object (Organisation).
+   *
+   * @return mixed
+   *   Either NULL or an array of commodity strings.
+   */
+  function getCommodities($emw) {
+    // Can't use property directly since government agencies do not have this field.
+    if (isset($emw->field_o_commodities)) {
+      $commodities = $emw->field_o_commodities->value();
+      if ($commodities) {
+        $result = array();
+        foreach ($commodities as $commodity) {
+          $result[] = $commodity->name;
+        }
+        return $result;
+      }
+    }
+    return NULL;
   }
 }
