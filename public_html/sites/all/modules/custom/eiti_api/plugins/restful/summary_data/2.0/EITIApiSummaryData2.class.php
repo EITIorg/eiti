1c50f8662e2dc4bae0e58979bdbb5f8fcfe01b7a
Madis Kure <kmadis@gmail.com>
2018-08-02 10:22:48 +0300

    EITIMS-8 API v2 (summary data ISO date for filters)

--- public_html/sites/all/modules/custom/eiti_api/plugins/restful/summary_data/2.0/EITIApiSummaryData2.class.php
+++ public_html/sites/all/modules/custom/eiti_api/plugins/restful/summary_data/2.0/EITIApiSummaryData2.class.php
@@ -23,4 +23,34 @@ class EITIApiSummaryData2 extends EITIApiSummaryData {
     return $public_fields;
   }
 
+  protected function parseRequestForListFilter() {
+    $filters = parent::parseRequestForListFilter();
+    foreach ($filters as $key => $filter) {
+      // Date to timestamp.
+      if (isset($filter['public_field'], $filter['value'][0]) && $filter['public_field'] == 'year_start') {
+        foreach ($filter['value'] as $k => $v) {
+          $filters[$key]['value'][$k] = strtotime($v);
+        }
+      }
+      if (isset($filter['public_field'], $filter['value'][0]) && $filter['public_field'] == 'year_end') {
+        foreach ($filter['value'] as $k => $v) {
+          $filters[$key]['value'][$k] = strtotime($v);
+        }
+      }
+      // Note that with ISO 8601 (YYYY-MM-DDThh:mm:ss+hh:mm) format the + symbol has to be encoded in the url (%2B).
+      // "2016-06-06T11:04:36%2B00:00" and "2016-06-06T11:04:36" give the same result.
+      if (isset($filter['public_field'], $filter['value'][0]) && $filter['public_field'] == 'created') {
+        foreach ($filter['value'] as $k => $v) {
+          $filters[$key]['value'][$k] = strtotime($v);
+        }
+      }
+      if (isset($filter['public_field'], $filter['value'][0]) && $filter['public_field'] == 'changed') {
+        foreach ($filter['value'] as $k => $v) {
+          $filters[$key]['value'][$k] = strtotime($v);
+        }
+      }
+    }
+    return $filters;
+  }
+
 }
