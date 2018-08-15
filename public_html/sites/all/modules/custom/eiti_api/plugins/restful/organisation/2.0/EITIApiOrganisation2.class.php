fff7fc4efbafaccb3f2b616b1ac2b8df369ad6f0
Madis Kure <kmadis@gmail.com>
2018-08-15 12:02:02 +0300

    EITIMS-8 API v2 (organisation, revenue)

--- public_html/sites/all/modules/custom/eiti_api/plugins/restful/organisation/2.0/EITIApiOrganisation2.class.php
+++ public_html/sites/all/modules/custom/eiti_api/plugins/restful/organisation/2.0/EITIApiOrganisation2.class.php
@@ -17,8 +17,27 @@ class EITIApiOrganisation2 extends EITIApiOrganisation {
     $public_fields = parent::publicFieldsInfo();
 
     // Expose data.
-    $public_fields['country']['process_callbacks'] = array('eitientity_implementing_country_get_iso2');
+    //$public_fields['country']['process_callbacks'] = array('eitientity_implementing_country_get_iso2');
+    $public_fields['country'] = array(
+      'callback' => array($this, 'getCountryApiUrl')
+    );
 
     return $public_fields;
+  }
+
+  /**
+   * Gets the implementing country API url.
+   */
+  function getCountryApiUrl($emw) {
+    if (isset($emw->country_id)) {
+      $country_id = $emw->country_id->value();
+      if (is_numeric($country_id)) {
+        $iso = eitientity_implementing_country_get_iso2($country_id);
+        if ($iso) {
+          return url('api/v2.0/implementing_country/' . $iso, array('absolute' => TRUE));
+        }
+      }
+    }
+    return NULL;
   }
 }
--- public_html/sites/all/modules/custom/eiti_api/plugins/restful/revenue/2.0/EITIApiRevenueStreams2.class.php
+++ public_html/sites/all/modules/custom/eiti_api/plugins/restful/revenue/2.0/EITIApiRevenueStreams2.class.php
@@ -11,6 +11,30 @@
 class EITIApiRevenueStreams2 extends EITIApiRevenueStreams {
 
   /**
+   * Overrides EITIApiRevenueStreams::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['id']['process_callbacks'][] = 'eiti_api_numeric_string_to_int';
+    $public_fields['revenue']['process_callbacks'][] = 'eiti_api_numeric_string_to_float';
+    $public_fields['type']['process_callbacks'][] = 'ucfirst';
+    unset($public_fields['report_status']);
+
+    $public_fields['label'] = array(
+      'property' => 'name',
+    );
+    $public_fields['gfs'] = array(
+      'callback' => array($this, 'getGfsApiUrl')
+    );
+    $public_fields['organisation'] = array(
+      'callback' => array($this, 'getOrganisationApiUrl')
+    );
+
+    return $public_fields;
+  }
+
+  /**
    * Overrides EITIApiRevenueStreams::getOrganisation().
    */
   public function getOrganisation($value_emw) {
@@ -20,5 +44,31 @@ class EITIApiRevenueStreams2 extends EITIApiRevenueStreams {
       $organisation->country_id = eitientity_implementing_country_get_iso2($organisation->country_id);
     }
     return $organisation;
+  }
+
+  /**
+   * Gets the gfs code API url.
+   */
+  function getGfsApiUrl($emw) {
+    if (isset($emw->gfs_code_id)) {
+      $gfs_code = $emw->gfs_code_id->value();
+      if (isset($gfs_code->id)) {
+        return url('api/v1.0/gfs_code/' . $gfs_code->id, array('absolute' => TRUE));
+      }
+    }
+    return NULL;
+  }
+
+  /**
+   * Gets the organisation API url.
+   */
+  function getOrganisationApiUrl($emw) {
+    if (isset($emw->organisation_id)) {
+      $organisation = $emw->organisation_id->value();
+      if (isset($organisation->id)) {
+        return url('api/v2.0/organisation/' . $organisation->id, array('absolute' => TRUE));
+      }
+    }
+    return NULL;
   }
 }
