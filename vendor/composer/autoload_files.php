222bbb909729f9d6e965249dc444a3e868d3ebcc
Madis Kure <kmadis@gmail.com>
2018-10-31 17:15:44 +0200

    EITIMS-36 mpdf update that hopefully fixes pdf display issues

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -10,4 +10,5 @@ return array(
     'a0edc8309cc5e1d60e3047b5df6b7052' => $vendorDir . '/guzzlehttp/psr7/src/functions_include.php',
     '5255c38a0faeba867671b61dfda6d864' => $vendorDir . '/paragonie/random_compat/lib/random.php',
     '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
+    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
 );
