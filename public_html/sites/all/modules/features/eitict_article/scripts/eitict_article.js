fcbc89716c2f52203009b4bac47332ae3591be68
Madis Kure <kmadis@gmail.com>
2019-07-09 16:39:52 +0300

    EITIMS-136 Article list filter updates

--- public_html/sites/all/modules/features/eitict_article/scripts/eitict_article.js
+++ public_html/sites/all/modules/features/eitict_article/scripts/eitict_article.js
@@ -1,7 +1,5 @@
 (function ($) {
 
-  // TODO: fix attach on ajax events.
-
   Drupal.behaviors.eitiArticles = {
     /**
      * Enables show/hide functionality for filters.
@@ -9,11 +7,38 @@
      */
     attach: function (context) {
 
-      $('.view-articles .view-filters').hide();
-      $('.view-articles .view-filters .views-exposed-widget').not('.views-submit-button').hide();
+      // On load.
+      $('.view-articles').once('filter').each(function() {
+        $(this).find('.view-filters .views-exposed-widget').not('.views-submit-button').hide();
+        var display_filter = false;
+        var f_field1 = $(this).find('.views-exposed-widgets .views-widget-filter-field_tags_tid .form-text');
+        var f_field2 = $(this).find('.views-exposed-widgets .views-widget-filter-combine .form-text');
+        if (f_field1.val() || f_field2.val()) {
+          display_filter = true;
+          $(this).find('.filter-toggle .filter').addClass('open');
+          $(this).find('.views-exposed-widgets .views-widget-filter-field_tags_tid').show();
+          $(this).find('.views-exposed-widgets .views-widget-filter-combine').show();
+        }
+        var display_date = false;
+        var d_field1 = $(this).find('.views-exposed-widgets .views-widget-filter-created .form-text');
+        var d_field2 = $(this).find('.views-exposed-widgets .views-widget-filter-created_1 .form-text');
+        if (d_field1.val() || d_field2.val()) {
+          display_date = true;
+          $(this).find('.filter-toggle .date').addClass('open');
+          $(this).find('.views-exposed-widgets .views-widget-filter-created').show();
+          $(this).find('.views-exposed-widgets .views-widget-filter-created_1').show();
+        }
 
-      // Filter.
-      $('.view-articles .filter-toggle .filter', context).click(function (e) {
+        if (display_filter || display_date) {
+          $(this).find('.view-filters').show();
+        }
+        else {
+          $(this).find('.view-filters').hide();
+        }
+      });
+
+      // Filter click.
+      $('.view-articles .filter-toggle .filter').once('filter-click').click(function (e) {
         if (!$(this).hasClass('open')) {
           $(this).addClass('open');
           $(this).closest('.view-articles').find('.view-filters').show();
@@ -30,8 +55,8 @@
         }
       });
 
-      // Date.
-      $('.view-articles .filter-toggle .date', context).click(function (e) {
+      // Date click.
+      $('.view-articles .filter-toggle .date').once('date-click').click(function (e) {
         if (!$(this).hasClass('open')) {
           $(this).addClass('open');
           $(this).closest('.view-articles').find('.view-filters').show();
