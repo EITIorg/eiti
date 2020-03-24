0aa123b9d7e847947980adb5553c0630c50a67ed
oleg.sabadash <oleg.sabadash@adm.ee>
2020-03-24 11:48:11 +0200

    [#feature/EITIMS-192] - updating logo pdf and the same footer for the first and others pages

--- public_html/sites/all/modules/custom/eiti_pdf/templates/eiti-pdf-footer.tpl.php
+++ public_html/sites/all/modules/custom/eiti_pdf/templates/eiti-pdf-footer.tpl.php
@@ -1,13 +1,10 @@
 <div class="pdf-footer" id="footer">
   <span class="name"><?php print t('EITI International Secretariat'); ?></span>
   <div>
-  <span class="phone"><?php print t('Phone: +47 222 00 800'); ?><?php print '  •  '?></span>
-  <span class="e-mail"><?php print t('Email: secretariat@eiti.org'); ?><?php print '  •  '?></span>
-  <span class="twitter"><?php print t('Twitter: @EITIorg'); ?><?php print '  •  '?></span>
-  <span class="website"><?php print t('www.eiti.org'); ?></span>
-  </div>
-  <div>
-  <span class="address"><?php print t('Address: Rådhusgata 26, 0151 Oslo, Norway'); ?><?php print '  •  '?></span>
-  <span class="post"><?php print t('P.O. Box: Postboks 340 Sentrum, 0101 Oslo, Norway'); ?></span><br>
+    <span class="phone"><?php print t('+47 222 00 800'); ?><?php print '  •  '?></span>
+    <span class="e-mail"><?php print t('secretariat@eiti.org'); ?><?php print '  •  '?></span>
+    <span class="twitter"><?php print t('Twitter: @EITIorg'); ?><?php print '  •  '?></span>
+    <span class="website"><?php print t('www.eiti.org'); ?><?php print '  •  '?></span>
+    <span class="address"><?php print t('Rådhusgata 26, 0151 Oslo, Norway'); ?></span>
   </div>
 </div>
