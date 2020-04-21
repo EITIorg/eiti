12d31daeeaee5b1604e737d3e61f1061a90c5ce3
oleg.sabadash <oleg.sabadash@adm.ee>
2020-04-21 13:12:05 +0300

    feature/EITIMS-195 update GTM ID to W57C5B4

--- public_html/sites/all/themes/eiti/templates/html.tpl.php
+++ public_html/sites/all/themes/eiti/templates/html.tpl.php
@@ -34,14 +34,14 @@
     );var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-P9HNP4B');</script>
+    })(window,document,'script','dataLayer','GTM-W57C5B4');</script>
   <!-- End Google Tag Manager -->
   <?php print $scripts; ?>
   <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us5.list-manage.com","uuid":"691e61806662528c68908252f","lid":"6aa20b6ee3"}) })</script>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P9HNP4B"
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W57C5B4"
                     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
   <div id="skip-link">
